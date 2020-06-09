ba6f961c9bdbb82ba68070166262d745869c1618
conorshort <cshort@tcd.ie>
2020-06-09 16:51:30 +0100

    add bustop model and set up db

--- WebApp/dublin_bus/settings.py
+++ WebApp/dublin_bus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(zg1l-9*n7tj%g=l)e$)c7^iq!v121t#$y8x+8!hv&9ag@(gh_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dublin_bus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangomess',
+        'USER': 'root',
+        'PASSWORD': 'd6A2zQyaSHWDvw2pE8RBNeG3',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- WebApp/routeplanner/models.py
+++ WebApp/routeplanner/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+import requests
+import pandas as pd
+import json
 
-# Create your models here.
+
+class Cat(models.Model):
+    idcat = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cat'
+
+
+class BusStop(models.Model):
+    stopid = models.IntegerField(primary_key=True)
+    displaystopid = models.IntegerField(blank=True, null=True)
+    shortname = models.CharField(max_length=200, blank=True, null=True)
+    shortnamelocalized = models.CharField(
+        max_length=200, blank=True, null=True)
+    fullname = models.CharField(
+        max_length=200, blank=True, null=True)
+    fullnamelocalized = models.CharField(
+        max_length=200, blank=True, null=True)
+    latitude = models.IntegerField(blank=True, null=True)
+    longitude = models.IntegerField(blank=True, null=True)
+    lastupdated = models.DateField(blank=True, null=True)
+    operator = models.CharField(max_length=20, blank=True, null=True)
+    op_type = models.IntegerField(blank=True, null=True)
+    routes = models.TextField(blank=True, null=True)
+
+    @classmethod
+    def update_all_stops():
+
+        r = requests.get(
+            'https://data.smartdublin.ie/cgi-bin/rtpi/busstopinformation?&format=json')
+
+        stops_json = json.loads(r.text)
+
+        stops = []
+        for stop in stops_json["results"]:
+
+            # Only add stops served by Dublin Bus ("bac")
+            bac_idx = 0
+            for op in stop["operators"]:
+                if op["name"] == "bac":
+                    break
+                else:
+                    bac_present += 1
+            else:
+                continue
+
+            cls(
+                stopid=stop["stopid"],
+                displaystopid=stop["displaystopid"],
+                shortname=stop["shortname"],
+                shortnamelocalized=stop["shortnamelocalized"],
+                fullname=stop["fullname"],
+                fullnamelocalized=stop["fullnamelocalized"],
+                latitude=stop["latitude"],
+                longitude=stop["longitude"],
+                lastupdated=stop["lastupdated"],
+                operator=stop["operators"][bac_idx]["name"],
+                op_type=stop["operators"][bac_idx]["operatortype"],
+                routes=stop["operators"][bac_idx]["routes"]
+            ).save()
+
+    class Meta:
+        managed = True
+        db_table = 'all_bus_stops'
