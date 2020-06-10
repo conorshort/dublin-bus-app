1929e89c4f19f5a1bac42e3dac8e479c6dcbc18f
conorshort <cshort@tcd.ie>
2020-06-10 15:12:29 +0100

    add bus data models

--- WebApp/routeplanner/models.py
+++ WebApp/routeplanner/models.py
@@ -1,17 +1,77 @@
 from django.db import models
-import requests
-import pandas as pd
-import json
+
+class BusStopManager(models.Manager):
+
+    def _get_all_bus_stop_data(self):
+        ''' Connect to smartdublin api and return a JSON of all bus stops '''
+
+        import requests
+        import json
+
+        print("Getting data...")
+
+        all_stops_URL = 'https://data.smartdublin.ie/cgi-bin/rtpi/busstopinformation?&format=json'
+
+        try:
+            r = requests.get(all_stops_URL, timeout=20)
+        except requests.exceptions.RequestException as e:
+            print("Something went wrong: could not connect to", all_stops_URL)
+            return None
+        else:
+            print("Data obtained.")
+            return json.loads(r.text)["results"]
+
+    def _format_date_for_django(self, date_str):
+        ''' Format a date-time str from smartdublin API for use in Django model'''
+        from datetime import datetime
+
+        date_obj = datetime.strptime(date_str, "%d/%m/%Y %H:%M:%S")
+        return date_obj.strftime("%Y-%m-%d")
+
+    def update_all_stops(self):
+        ''' Find all Dublin Bus stops currently in use and update the database '''
+        stops_json = _get_all_bus_stop_data()
+
+        print("Adding to db...")
+        bus_stops = []
+        for stop in stops_json:
+
+            # Each stop can be served by one or more operators
+            # Only add stops and routes served by Dublin Bus ("bac")
+            bac_idx = 0
+            for op in stop["operators"]:
+                if op["name"] == "bac":
+                    break
+                else:
+                    bac_idx += 1
+            else:
+                continue
+
+            last_update_str = __format_date_for_django(stop["lastupdated"])
+
+            # Make an instance of BusStop
+            bus_stops.append(BusStop(
+                stopid=stop["stopid"],
+                displaystopid=stop["displaystopid"],
+                shortname=stop["shortname"],
+                shortnamelocalized=stop["shortnamelocalized"],
+                fullname=stop["fullname"],
+                latitude=stop["latitude"],
+                longitude=stop["longitude"],
+                lastupdated=last_update_str,
+                operator=stop["operators"][bac_idx]["name"],
+                op_type=stop["operators"][bac_idx]["operatortype"],
+                routes=stop["operators"][bac_idx]["routes"]
+            ))
+
+        # Add all BusStop instances to the DB
+        self.bulk_create(
+            bus_stops, batch_size=100, ignore_conflicts=True)
+        print("\nDone")
+t
 
 
-class Cat(models.Model):
-    idcat = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=45, blank=True, null=True)
-    age = models.IntegerField(blank=True, null=True)
 
-    class Meta:
-        managed = False
-        db_table = 'cat'
 
 
 class BusStop(models.Model):
@@ -22,51 +82,73 @@ class BusStop(models.Model):
         max_length=200, blank=True, null=True)
     fullname = models.CharField(
         max_length=200, blank=True, null=True)
-    fullnamelocalized = models.CharField(
-        max_length=200, blank=True, null=True)
-    latitude = models.IntegerField(blank=True, null=True)
-    longitude = models.IntegerField(blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     lastupdated = models.DateField(blank=True, null=True)
     operator = models.CharField(max_length=20, blank=True, null=True)
     op_type = models.IntegerField(blank=True, null=True)
     routes = models.TextField(blank=True, null=True)
 
-    @classmethod
-    def update_all_stops():
-
-        r = requests.get(
-            'https://data.smartdublin.ie/cgi-bin/rtpi/busstopinformation?&format=json')
-
-        stops_json = json.loads(r.text)
-
-        stops = []
-        for stop in stops_json["results"]:
-
-            # Only add stops served by Dublin Bus ("bac")
-            bac_idx = 0
-            for op in stop["operators"]:
-                if op["name"] == "bac":
-                    break
-                else:
-                    bac_present += 1
-            else:
-                continue
-
-            cls(
-                stopid=stop["stopid"],
-                displaystopid=stop["displaystopid"],
-                shortname=stop["shortname"],
-                shortnamelocalized=stop["shortnamelocalized"],
-                fullname=stop["fullname"],
-                fullnamelocalized=stop["fullnamelocalized"],
-                latitude=stop["latitude"],
-                longitude=stop["longitude"],
-                lastupdated=stop["lastupdated"],
-                operator=stop["operators"][bac_idx]["name"],
-                op_type=stop["operators"][bac_idx]["operatortype"],
-                routes=stop["operators"][bac_idx]["routes"]
-            ).save()
+    # Model manager
+    objects = BusStopManager()
 
     class Meta:
         managed = True
         db_table = 'all_bus_stops'
+
+
+
+
+
+class RouteShapeManager(models.Manager):
+    def get_shape_json_by_shape_id(self, shape_id):
+        shape = RouteShape.objects.filter(shape_id=shape_id)
+        for point in shape:
+            d = {"lat": point["shape_pt_lat"],
+                "lon": point["shape_pt_lon"]}
+
+    def update_all_shapes(self):
+        import pandas as pd
+        print("Getting data...")
+        df = pd.read_csv("C:/Users/cls15/Google Drive/Comp Sci/Research Practicum/Code/dublin-bus-app/Models/Data Cleaning/shapes.txt")
+        df_records = df.to_dict('records')
+        
+        print("Adding to db...")
+        model_instances = [RouteShape(
+            unique_point_id=record["shape_id"] + "Seq:" + record["shape_pt_sequence"],
+            shape_id=record["shape_id"],
+            shape_pt_lat=record["shape_pt_lat"],
+            shape_pt_lon=record["shape_pt_lon"],
+            shape_pt_sequence=record["shape_pt_sequence"],
+            shape_dist_traveled=record["shape_dist_traveled"]
+        ) for record in df_records]
+
+        self.bulk_create(model_instances,
+                         batch_size=100,
+                         ignore_conflicts=True)
+        print("Done")
+
+
+
+
+
+class RouteShape(models.Model):
+    unique_point_id = models.CharField(max_length=30)
+    shape_id = models.CharField(max_length=30)
+    shape_pt_lat = models.FloatField(blank=True, null=True)
+    shape_pt_lon = models.FloatField(blank=True, null=True)
+    shape_pt_sequence = models.IntegerField()
+    shape_dist_traveled = models.FloatField(blank=True, null=True)
+
+    # Model manager
+    objects = RouteShapeManager()
+
+    def __repr__(self):
+        return f'''Id: {self.shape_id};
+                   Coords: {self.shape_pt_lat}, {self.shape_pt_lon};
+                   Seq: {self.shape_pt_sequence}'''
+
+    class Meta:
+        managed = True
+        db_table = 'routeshapes'
+        unique_together = (("shape_id", "shape_pt_sequence"),)
