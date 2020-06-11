2d8595c5f1934d7ee073849f4fc2c70ba835de16
Dan O'Beirne <obeirnedan@gmail.com>
2020-06-11 11:44:09 +0100

    Added title to pages

--- WebApp/routeplanner/views.py
+++ WebApp/routeplanner/views.py
@@ -25,10 +25,10 @@ def home(request):
     return render(request, 'routeplanner/home.html', context)
 
 def stops(request):
-    return render(request, 'routeplanner/stops.html')
+    return render(request, 'routeplanner/stops.html', {'title' : 'Stops'} )
 
 def routes(request):
-    return render(request, 'routeplanner/routes.html')
+    return render(request, 'routeplanner/routes.html', {'title' : 'Stops'} )
 
 def leapcard(request):
-    return render(request, 'routeplanner/leapcard.html')
+    return render(request, 'routeplanner/leapcard.html', {'title' : 'Leapcard'})
