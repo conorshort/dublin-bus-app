22488e0ac8922ea7078474f3ce78b9904463fc3e
conorshort <cshort@tcd.ie>
2020-06-11 14:24:32 +0100

    rename parse_route_sequences

---
+++ Models/Data Cleaning/parse_route_sequences.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("C:/Users/cls15/Google Drive/Comp Sci/Research Practicum/Code/dublin-bus-app/Models/Data Cleaning/routesequences.csv")
+
+df = df[df.Operator == "Dublin Bus"]
+df = df.drop(columns=['RouteData', "AtcoCode"])
+
+df['HasPole'].loc[df['HasPole'] == "Pole"] = 1
+df['HasPole'].loc[df['HasPole'] == "No Pole"] = 0
+df['HasPole'].loc[df['HasPole'] == "Unknown"] = ""
+
+df['HasShelter'].loc[df['HasShelter'] == "Shelter"] = 1
+df['HasShelter'].loc[df['HasShelter'] == "No Shelter"] = 0
+df['HasShelter'].loc[df['HasShelter'] == "Unknown"] = np.nan
+
+df = df.rename(columns={'Direction': 'DirectionInbound'})
+df['DirectionInbound'].loc[df['DirectionInbound'] == "Outbound"] = 0
+df['DirectionInbound'].loc[df['DirectionInbound'] == "Inbound"] = 1
+
+
+print(df[df["HasShelter"].isna()])
