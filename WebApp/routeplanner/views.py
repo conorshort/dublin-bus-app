8bd902d4642d76b9247743b83d2461e54cb44366
Dan O'Beirne <obeirnedan@gmail.com>
2020-06-12 17:14:54 +0100

    added weather to all urls

--- WebApp/routeplanner/views.py
+++ WebApp/routeplanner/views.py
@@ -17,22 +17,20 @@ dummy_bus_content = [
         'time': '15:55'
     }
 ]
+API = '83e8d16b48f83517a5d89158fc88656e'
+URL = "http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=" + API
+try:
+    city_weather = requests.get(url=URL).json() #sends request to API and resturns weather data in Json
+except:
+    print("Error, Weather Data Not Recieved")        
 
+weather = {
+    'temperature' : int(city_weather['main']['temp'] - 273.15),
+    'description' : city_weather['weather'][0]['description'],
+    'icon' : city_weather['weather'][0]['icon']
+}
 
 def home(request):
-
-    API = '83e8d16b48f83517a5d89158fc88656e'
-    URL = "http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=" + API
-    try:
-        city_weather = requests.get(url=URL).json() #sends request to API and resturns weather data in Json
-    except:
-        print("Error, Weather Data Not Recieved")        
-    
-    weather = {
-        'temperature' : int(city_weather['main']['temp'] - 273.15),
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon']
-    }
 
     context = {
         'busses': dummy_bus_content,
@@ -43,12 +41,26 @@ def home(request):
     return render(request, 'routeplanner/home.html', context)
 
 def stops(request):
-    return render(request, 'routeplanner/stops.html', {'title' : 'Stops'} )
+
+    context = {
+            'weather': weather,
+            'title':'stops'
+        }
+
+    return render(request, 'routeplanner/stops.html', context)
 
 def routes(request):
-    return render(request, 'routeplanner/routes.html', {'title' : 'Routes'} )
+    context = {
+            'weather': weather,
+            'title':'stops'
+        }
+    return render(request, 'routeplanner/routes.html', context)
 
 def leapcard(request):
-    return render(request, 'routeplanner/leapcard.html', {'title' : 'Leapcard'})
+    context = {
+            'weather': weather,
+            'title':'stops'
+        }
+    return render(request, 'routeplanner/leapcard.html', context,)
 
   
