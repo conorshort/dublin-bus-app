20db00fa3d0c5cbb493ca679194b5c6275c08adb
wenghsinping <ohmyhappy77@gmail.com>
2020-06-16 00:35:17 +0100

    create api for getting stops within given radius

--- WebApp/api/views.py
+++ WebApp/api/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from .models import BusStop
 from .serializers import BusStopSerializer
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
+from django.http import HttpResponse
 
 class BusStopViewSet(viewsets.ModelViewSet):
     queryset = BusStop.objects.all()
     serializer_class = BusStopSerializer
 
-    # def get_queryset(self):
-    #     longitude = self.request.query_params.get('longitude')
-    #     latitude= self.request.query_params.get('latitude')
-    #     radius = self.request.query_params.get('radius')
+    def get_queryset(self):
 
-    #     location = Point(longitude, latitude)
-    #     queryset = BusStop.objects.filter(location__distance_lte=(location, D(m=distance))).distance(location).order_by('distance')
+        longitude = self.request.query_params.get('longitude')
+        latitude= self.request.query_params.get('latitude')
+        radius = self.request.query_params.get('radius')
+        
+        # SQL statement that will find the stops that are within given radius
+        sql = "SELECT *, ( 3959 * acos ( cos ( radians( %s ) )\
+            * cos( radians( s.latitude ) )\
+            * cos( radians( s.longitude ) \
+                - radians( %s ) )\
+            + sin ( radians( %s ) )\
+            * sin( radians( s.latitude ) ))) \
+            AS distance \
+            FROM dublin_bus_test.all_bus_stops \
+            as s HAVING distance < %s \
+            ORDER BY distance;" % (latitude, longitude, latitude, radius) 
 
-    #     return queryset
+
+        queryset = BusStop.objects.raw(sql)
+        serializer = BusStopSerializer(queryset, many=True)
+
+        return serializer.data
+
+    
