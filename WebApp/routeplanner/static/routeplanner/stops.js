93d3f58a4ed7376a7f642dc80a6348fec1202921
Dan O'Beirne <obeirnedan@gmail.com>
2020-07-09 14:09:56 +0100

    unit fixed for stop distances

--- WebApp/routeplanner/static/routeplanner/stops.js
+++ WebApp/routeplanner/static/routeplanner/stops.js
@@ -26,7 +26,7 @@ function showStops(){
         content = '';
         $.each(data, function (i, stop) {
             // Get distance from centre location to every stop in kilometers
-            dist_kms = distance(centreLocation[0],stop.latitude,centreLocation[1],stop.longitude, K)
+            dist_kms = distance(centreLocation[0],stop.latitude,centreLocation[1],stop.longitude, 'K')
             content += renderListItem(stop,dist_kms);
             markStopsOnMap(stop)
         });
